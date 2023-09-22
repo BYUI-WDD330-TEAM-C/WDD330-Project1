48a53ed82a036e2a5060e4f620d5a9ba02364446
brendatondin <bentotbrenda@gmail.com>
2023-09-22 00:01:07 -0300

    deleted comments

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,45 +1,14 @@
-// import { getLocalStorage } from "./utils.mjs";
-
-// function renderCartContents() {
-//   const cartItems = getLocalStorage("so-cart");
-//   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-//   document.querySelector(".product-list").innerHTML = htmlItems.join("");
-// }
-
-// function cartItemTemplate(item) {
-//   const newItem = `<li class="cart-card divider">
-//   <a href="#" class="cart-card__image">
-//     <img
-//       src="${item.Image}"
-//       alt="${item.Name}"
-//     />
-//   </a>
-//   <a href="#">
-//     <h2 class="card__name">${item.Name}</h2>
-//   </a>
-//   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-//   <p class="cart-card__quantity">qty: 1</p>
-//   <p class="cart-card__price">$${item.FinalPrice}</p>
-// </li>`;
-
-//   return newItem;
-// }
-
-// renderCartContents();
 
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  // Get cart items from local storage
+
   const cartItems = getLocalStorage("so-cart");
 
-  // Check if cartItems is an array, or initialize it as an empty array if it's not
   const cartItemsArray = Array.isArray(cartItems) ? cartItems : [];
 
-  // Generate HTML for each cart item using cartItemTemplate
   const htmlItems = cartItemsArray.map((item) => cartItemTemplate(item));
 
-  // Set the innerHTML of the product list element
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,3 @@
-// import { setLocalStorage } from "./utils.mjs";
-// import { findProductById } from "./productData.mjs";
-
-// function addProductToCart(product) {
-//   setLocalStorage("so-cart", product);
-// }
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
-
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
-
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
@@ -29,21 +12,17 @@ function addProductToCart(product) {
     cart = [];
   }
 
-  // Add the product to the cart
   cart.push(product);
 
-  // Save the updated cart back to LocalStorage
   setLocalStorage("so-cart", cart);
 }
 
-// Add to cart button event handler
 async function addToCartHandler(e) {
-  e.preventDefault(); // Prevent form submission
+  e.preventDefault(); 
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// Add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
