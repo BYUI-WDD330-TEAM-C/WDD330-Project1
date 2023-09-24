3698fdd5f06883b4d02daf6bb2f1feff3ba7d93d
patrickarmani <arm22001@byui.edu>
2023-09-23 23:27:16 -0300

    "cart-broken"

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,33 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+//creating a new getLocalStorage
+
+/*===============================================*/
+
+if (!getLocalStorage("so-cart")) {
+  setLocalStorage("so-cart", []);
 }
+/*===============================================*/
+
+function addProductToCart(product) {
+   //creating new variable: let cart
+  let cart = getLocalStorage("so-cart");
+
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  cart.push(product);
+
+  setLocalStorage("so-cart", cart);
+}
+  /*setLocalStorage("so-cart", product); 
+}*/
+
 // add to cart button event handler
 async function addToCartHandler(e) {
+  e.preventDefault();
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
@@ -14,3 +36,8 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+  
+
+
+
