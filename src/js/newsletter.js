18598db82cb33c37c8498f902bb4970ceb7e051b
brendatondin <bentotbrenda@gmail.com>
2023-10-09 19:22:06 -0300

    fixed display 4 tents and format of files

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const newsletterForm = document.getElementById("newsletter-form");
-    const emailInput = document.getElementById("email");
-  
-    newsletterForm.addEventListener("submit", function (e) {
-      e.preventDefault();
-      const email = emailInput.value;
-      localStorage.setItem("newsletterEmail", email);
-      
-      alert("Thank you for subscribing to our newsletter!");
-  
-      emailInput.value = "";
-    });
+  const newsletterForm = document.getElementById("newsletter-form");
+  const emailInput = document.getElementById("email");
+
+  newsletterForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const email = emailInput.value;
+    localStorage.setItem("newsletterEmail", email);
+
+    alert("Thank you for subscribing to our newsletter!");
+
+    emailInput.value = "";
   });
-  
+});
