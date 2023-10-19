d303d3c2d3b93793f83a734bc28da89513609426
brendatondin <bentotbrenda@gmail.com>
2023-10-19 14:57:30 -0300

    fixing and style login page

--- src/js/signup.js
+++ src/js/signup.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       alert("Registration successful!");
 
-      window.location.href = "index.html";
+      window.location.href = "../index.html";
     }
   });
 
